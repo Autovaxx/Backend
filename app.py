f3cef96579db9175f49ffc4d1b61a453b6c4c1da
smarceta <smarceta202@gmail.com>
2022-09-26 10:03:26 -0400

    Updated chrome driver options

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
-#from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
 
 # serv = Service("chromedriver.exe")
 # opts = webdriver.ChromeOptions()
@@ -15,7 +15,12 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--disable-gpu")
+## might not be needed
+chrome_options.add_argument("--remote-debugging-port=9222")
+chrome_options.add_argument('--window-size=1920x1480')
+
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 # Connecting to this page
@@ -45,7 +50,7 @@ details_obj = [{
     "weather": weather_details
 }]
 
-# Closing selenim instance after data is fetched/process is complete
+# Closing selenim instance after data is fetched/process is closed
 driver.close()
 
 # Converting object to json
