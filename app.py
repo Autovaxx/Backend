2514a3f357062addbd0b7c9a5f4115151c6b74f8
smarceta <smarceta202@gmail.com>
2022-09-26 11:01:36 -0400

    test

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 import json
-# from selenium import webdriver
+from selenium import webdriver
+import os
+
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
-# import os
+
 # from selenium.webdriver.chrome.service import Service
+# serv = Service("chromedriver.exe")
+# opts = webdriver.ChromeOptions()
+# driver = webdriver.Chrome(service=serv, options=opts)
 
-# # serv = Service("chromedriver.exe")
-# # opts = webdriver.ChromeOptions()
-# # driver = webdriver.Chrome(service=serv, options=opts)
+# Setting chrome options
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument('--no-sandbox')
 
-# # Setting chrome options
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument('--no-sandbox')
-# ## might not be needed
-# chrome_options.add_argument('--window-size=1920x1480')
-
-# driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 # # Connecting to this page
-# driver.get("https://www.google.com/search?q=mississauga+weather&rlz=1C1UEAD_enCA995CA995&oq=miss&aqs=chrome.0.69i59j46i433i512j69i57j69i59j69i61j69i65j69i61j69i60.520j0j7&sourceid=chrome&ie=UTF-8")
+driver.get("https://www.google.com/search?q=mississauga+weather&rlz=1C1UEAD_enCA995CA995&oq=miss&aqs=chrome.0.69i59j46i433i512j69i57j69i59j69i61j69i65j69i61j69i60.520j0j7&sourceid=chrome&ie=UTF-8")
 
 # # # Script Variables
 # # city = 'Mississauga weather'
@@ -47,17 +46,18 @@ import json
 #     "date_time": date_time,
 #     "weather": weather_details
 # }]
-details_obj = [{
-    "location": "HI",
-    "date_time": "BYE",
-    "weather": "YO"
-}]
+# details_obj = [{
+#     "location": "HI",
+#     "date_time": "BYE",
+#     "weather": "YO"
+# }]
 # Closing selenim instance after data is fetched/process is closed
 # driver.close()
 
 # Converting object to json
-details_obj_json = json.dumps(details_obj)
+details_obj_json = json.dumps(driver.page_source)
 
 # Printing object so its in the I/O stream for node to fetch
-print(details_obj_json)
+print(details_obj_json[10])
 
+driver.close()
