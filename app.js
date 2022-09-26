eb53ee8de26bd7c7da573e016f12753923acb3b7
smarceta <smarceta202@gmail.com>
2022-09-26 11:11:08 -0400

    21

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/pyscript',  (req, res) => {
     pythonOutput.on('exit', (code) => {
         console.log(`EXIT CODE: ${code}`)
 
-        res.send(dataFromPython)
+        res.json(dataFromPython)
     } )
 })
 
